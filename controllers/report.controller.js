2237857b1827c16c3d0cbb6d71361be00628dc96
Justin Graber <jgraberdev@gmail.com>
2017-02-28 08:35:16 -0600

    added start npm script. Mocked up report router

--- controllers/report.controller.js
+++ controllers/report.controller.js
@@ -4,23 +4,8 @@ const reportRouter   = express.Router();
 const reportModel    = require('../models/report.model');
 const reportSetModel = require('../models/report-set.model');
 
-TeamRouter.get('/', function(req, res) {
-  Team.find({}, function(err, teams) {
-    res.json({teams});
-  });
+reportRouter.get('/query', function(req, res) {
+
 });
 
-TeamRouter.post('/', function (req, res) {
-  const teamModel = new Team({
-    name: 'Innosol Test Team',
-    id: Common.generateObjectId()
-  });
-
-  teamModel.save(function(err) {
-    if (err) throw err;
-    console.log('team saved successfully');
-    res.json({ success: true });
-  });
-});
-
-module.exports = TeamRouter;
+module.exports = reportRouter;
