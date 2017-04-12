5b9d10c7459bccb389c8c713947de5b3c5f83f85
Justin Graber <jgraberdev@gmail.com>
2017-04-11 20:27:31 -0500

    Added missing files

---
+++ models/report-log.model.js
@@ -0,0 +1,18 @@
+// get an instance of mongoose and mongoose.Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ReportLogSchema = new Schema({
+  date: Date,
+  userId: mongoose.Schema.Types.ObjectId,
+  reportId: mongoose.Schema.Types.ObjectId
+});
+
+ReportLogSchema.virtual('id').get(function() {
+  return this._id.toString();
+});
+
+// set up a mongoose model and pass it using module.exports
+module.exports = mongoose.model('ReportLog', ReportLogSchema);
+
+
